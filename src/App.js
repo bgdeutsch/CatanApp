d952fdc413953df98ed99e67b5832e0076ad0262
Brian Deutsch <bgdeutsch@gmail.com>
2018-11-11 15:00:20 -0500

    Adds react-https-redirect library

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Scoreboard from './features/scoreboard';
 import GameDetails from './features/game';
 import GameForm from './features/game-form';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 const theme = createMuiTheme({
   palette: {
@@ -21,18 +22,20 @@ const theme = createMuiTheme({
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <Router>
-          <div>
-            <Header />
-            <div className='container'>
-              <Route exact path='/' component={ Scoreboard } />
-              <Route exact path='/game/:id' component={ GameDetails } />
-              <Route exact path='/create' component={ GameForm } />
+      <HttpsRedirect>
+        <MuiThemeProvider theme={theme}>
+          <Router>
+            <div>
+              <Header />
+              <div className='container'>
+                <Route exact path='/' component={ Scoreboard } />
+                <Route exact path='/game/:id' component={ GameDetails } />
+                <Route exact path='/create' component={ GameForm } />
+              </div>
             </div>
-          </div>
-        </Router>
-      </MuiThemeProvider>
+          </Router>
+        </MuiThemeProvider>
+      </HttpsRedirect>
     );
   }
 }
